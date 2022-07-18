c274c146fc855d848342facaa953108488e97485
KornoosVildberg <ryzhikov_andrej@list.ru>
2022-07-18 21:00:32 +0300

    new metod: def button

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
+from distutils.log import info
+from email import message
 import telebot
-import configure
-import types
+from telebot import types
 
 bot = telebot.TeleBot('5360883455:AAHk4kyImiqL5VS7Vd4Xwz9AmCVubGwfut4')
 
-@bot.message_handler(commands=['get_info','info'])
-def get_user_info(message):
-    markup_inline = types.InlineKeyboardMarkup()
-    item_yes = types.InlineKeyboardButton(text='Да', callback_data = 'yes')
-    item_no = types.InlineKeyboardButton(text='Нет', callback_data = 'no')
 
-    markup_inline.add(item_yes, item_no)
-    bot.send_message(message.chat.id, 'Информация о пользователе',
-        reply_markup=markup_inline
-    )
-
-@bot.callback_query_handler(func = lambda call: True)
-def answer(call):
-    if call.data == 'yes':
-        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def welcome(message):
-    sti = open('Pic/welcome.webp', 'rb')
+    sti = open('Pic/testing.webp', 'rb')
     bot.send_sticker(message.chat.id, sti)
 
     bot.send_message(message.chat.id, 
@@ -31,8 +16,46 @@ def welcome(message):
     format(message.from_user, bot.get_me()),parse_mode='html'
     )
 
-@bot.message_handler(content_types=['text'])
-def text(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['menu'])
+def menu(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    item_weather = types.InlineKeyboardButton(text = 'Погода', callback_data = 'weather')
+    item_about = types.InlineKeyboardButton(text = 'О пользователе', callback_data = 'about')
+    item_guide = types.InlineKeyboardButton(text = 'Справочник', callback_data = 'guide')
+
+    markup_inline.add(item_about, item_guide, item_weather)
+    bot.send_message(message.chat.id, 
+    'Меню',
+    reply_markup = markup_inline
+    )
+
+@bot.callback_query_handler(func = lambda call: True)
+def answer(call):
+    if call.data == 'weather':
+        bot.send_message(call.message.chat.id,
+        'test')
+
+    elif call.data == 'about':
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard = True)
+        item_id = types.KeyboardButton('Мой ID')
+        item_username = types.KeyboardButton('Мой ник')
+        markup_reply.add(item_id, item_username)
+        bot.send_message(call.message.chat.id, 'Выберите необходимый пункт.',
+        reply_markup= markup_reply
+        )
+
+    elif call.data == 'guide':
+        bot.send_message(call.message.chat.id,
+        'test')
+
+
+@bot.message_handler(content_types = ['text'])
+def get_about(message):
+    if message.text == 'Мой ID':
+        bot.send_message(message.chat.id,
+        f'Ваш ID: {message.from_user.id}')
+    elif message.text == 'Мой ник':
+        bot.send_message(message.chat.id,
+        f'Ваш ник: {message.from_user.first_name} {message.from_user.last_name}')
 
 bot.polling(none_stop = True, interval = 0) 
